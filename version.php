c292d5041f7e896fb387beb75d365e9b03ee7908
Niels Gandraß <niels@gandrass.de>
2023-08-14 14:02:51 +0200

    Release version 0.4.4 (2023081400)

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'quiz_archiver';
-$plugin->release = '0.4.3';
-$plugin->version = 2023080300;
+$plugin->release = '0.4.4';
+$plugin->version = 2023081400;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_BETA;
