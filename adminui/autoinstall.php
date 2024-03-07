70d3d1a4ed88cace586cd8177937b183af841488
Niels Gandraß <niels@gandrass.de>
2024-03-07 12:12:54 +0100

    autoinstall: Make PHPDoc happy again ;)

--- adminui/autoinstall.php
+++ adminui/autoinstall.php
@@ -22,6 +22,7 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or late
  */
 
+/** @var bool Disables output buffering */
 const NO_OUTPUT_BUFFERING = true;
 
 require_once(__DIR__.'/../../../../../config.php');
