ea9ae2ed1eaf275fb01ace2b687ea391477daefe
Niels Gandraß <niels@gandrass.de>
2024-05-08 11:33:48 +0200

    Release development version v1.2.6+2024050800

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'quiz_archiver';
-$plugin->release = '1.2.6';
-$plugin->version = 2024042900;
+$plugin->release = '1.2.6+2024050800';
+$plugin->version = 2024050800;
 $plugin->requires = 2022112800;
 $plugin->supported = [401, 404];
 //$plugin->incompatible = 402;
