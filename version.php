b3dad7cbfc2ee16323ebe8f39a5a47f31200d8b9
Niels Gandraß <niels@gandrass.de>
2026-08-03 15:11:00 +0200

    Bump version once again to get latest settings populated

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die(); // @codeCoverageIgnore
 
 $plugin->component = 'quiz_archiver';
 $plugin->release = '4.1.0';
-$plugin->version = 2026052201;
+$plugin->version = 2026052202;
 $plugin->requires = 2024100700;
 $plugin->supported = [405, 502]; // X meta-supported-moodle{4.5 - 5.2} meta-supported-php{8.1 - 8.4}.
 $plugin->maturity = MATURITY_STABLE;
